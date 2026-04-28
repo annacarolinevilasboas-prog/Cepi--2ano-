23e141de00a82d8bced050eec33b65e56cb0407d
annacarolinevilasboas-prog <annacarolinevilasboas@gmail.com>
2026-04-28 11:35:41 -0300

    Update Funcionario.cs

--- Projetos/Funcionario.cs
+++ Projetos/Funcionario.cs
@@ -1,1 +1,15 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anna2A
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double Salario;
+    }
+}
